82653f0e5df910110e376cedcf1fa6e2648d1754
Alex Borsbach <alexborsbach@Alexs-MacBook-Pro.local>
2019-10-14 19:35:44 -0500

    working news article fetch

--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react'
 
 class List extends Component {
-    // write componentDidMount function
-    // call "this.props.fetchArticles"
+  // write componentDidMount function
+  // call "this.props.fetchArticles"
+  componentDidMount() { 
+    this.props.fetchArticles()
+  }
 
-    render() {
-        return (
-            <div>
-                {this.props.articles.map(a => {
-                    return <p>{a.title}</p>
-                })}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        {this.props.articles.map(a => {
+          return <p>{a.title}</p>
+        })}
+      </div>
+    )
+  }
 }
 
 export default List
